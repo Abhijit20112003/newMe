384465c1e8b275e47156fd1c4ecb4b07d617cebf
Abhijit Roy <roya51788@gmail.com>
2023-01-03 16:04:58 +0530

    Update index.tsx
    adding dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { Button } from "@nextui-org/react";
+import React, { useState } from "react";
 import { NextPage } from "next";
 
 const Home:NextPage= () => {
   const router = useRouter();
   return (
     <>
+      <div className={darkMode ? 'dark-mode' :
+  'light-mode'}>
+   <button onClick={() => setDarkMode(!darkMode)}>Toggle Dark Mode</button>
+    </div>
       <div>
         <div className=" text-6xl mx-20 font-thin  mt-60">
           Transform your resolutions into reality with <br /> our easy-to-use platform
